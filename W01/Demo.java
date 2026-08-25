84b9d2dee47d9828f650cc527603c7777a223c8f
Kennaoui <kennaoui@luc.edu>
2026-08-25 17:00:28 +0000

    create Demo.java

---
+++ W01/Demo.java
@@ -0,0 +1,12 @@
+public class Demo { 
+
+	public static void main(String[] args) {
+		int count = 3;
+		double average = 87.5;
+		String word1 = "COMP 272";
+	       	String word2 = "COMP 272";	
+		System.out.println(word1 == word2);
+	}
+
+
+}
